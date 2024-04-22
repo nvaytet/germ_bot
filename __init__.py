40fcad36b089a05ec3d32a2f1b2828175e070e68
Neil Vaytet <neil.vaytet@esss.se>
2024-04-22 11:04:43 +0200

    cleanup

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-# flake8: noqa F401
-
 from .bot import Bot
+
+__all__ = ["Bot"]
--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Literal, Union
 
 import numpy as np
 
@@ -8,9 +7,12 @@ import numpy as np
 class Bot:
     """
     This is the bot that will be instantiated for the competition.
+
+    The pattern can be either a numpy array or a path to an image.
     """
 
     def __init__(self):
         self.name = "GramNegative"  # This is your team name
         self.color = None
         self.pattern = np.random.randint(0, 2, size=(14, 14))
+        # self.pattern = "filler.png"
