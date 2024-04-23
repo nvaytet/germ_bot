64d0564734b5598dd3a331f2b495d5b2dbf09b56
Neil Vaytet <neil.vaytet@esss.se>
2024-04-23 23:56:57 +0200

    update

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
+from typing import Optional, Tuple
 
-
+from cholerama import helpers
 import numpy as np
 
 
@@ -12,12 +13,14 @@ class Bot:
     """
 
     def __init__(self):
-        self.name = "GramNegative"  # This is your team name
+        self.name = "YeastieBoys"  # This is your team name
         self.color = None
         self.pattern = np.random.randint(0, 2, size=(14, 14))
         # self.pattern = "filler.png"
 
-    def run(self, iteration, board, tokens):
+    def run(
+        self, iteration: int, board: np.ndarray, tokens: int
+    ) -> Optional[Tuple[np.ndarray, np.ndarray]]:
         """
         This method will be called by the game engine on each iteration.
 
@@ -29,8 +32,19 @@ class Bot:
             The current state of the board.
         tokens : list
             The list of tokens on the board.
+
+        Returns:
+        -------
+        tuple
+            A tuple containing the x and y coordinates of the new cells.
         """
         if tokens >= 5:
-            x = np.array([1, 2, 0, 1, 2]) + np.random.randint(10, 450)
-            y = np.array([2, 1, 0, 0, 0]) + np.random.randint(10, 350)
+            # Pick a random empty patch of size 3x3
+            empty_patches = helpers.find_empty_patches(board, (3, 3))
+            npatches = len(empty_patches)
+            if npatches == 0:
+                return None
+            ind = np.random.randint(0, npatches)
+            x = np.array([1, 2, 0, 1, 2]) + empty_patches[ind, 1]
+            y = np.array([2, 1, 0, 0, 0]) + empty_patches[ind, 0]
             return x, y
